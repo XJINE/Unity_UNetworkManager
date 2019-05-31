dc18a39aee40df55b2049a71e4c3d4138d764687
XJINE <xjine.andorelseif@gmail.com>
2019-05-31 12:50:59 +0900

    Add UNetworkManagerSettings.

---
+++ Assets/Packages/UNetworkManager/UNetworkManagerSettings.cs
@@ -0,0 +1,44 @@
+﻿using static UNetworkManager;
+
+[System.Serializable]
+public class UNetworkManagerSettings
+{
+    // NOTE:
+    // This class is mainly used for Save & Load settings.
+
+    #region Field
+
+    public string   networkAddress;
+    public int      networkPort;
+    public bool     autoStart;
+    public UNetType autoStartType;
+
+    #endregion Field
+
+    #region Method
+
+    public void Save(UNetworkManager uNetworkManager) 
+    {
+        this.networkAddress = uNetworkManager.networkAddress;
+        this.networkPort    = uNetworkManager.networkPort;
+        this.autoStart      = uNetworkManager.autoStart;
+        this.autoStartType  = uNetworkManager.autoStartType;
+    }
+
+    public void Load(UNetworkManager uNetworkManager) 
+    {
+        bool active = uNetworkManager.isNetworkActive;
+
+        if (active)
+        {
+            uNetworkManager.Stop();
+        }
+
+        uNetworkManager.networkAddress = this.networkAddress;
+        uNetworkManager.networkPort    = this.networkPort;
+        uNetworkManager.autoStart      = this.autoStart;
+        uNetworkManager.autoStartType  = this.autoStartType;
+    }
+
+    #endregion Method
+}
